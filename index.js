42b347f8d9187a37536613d7496ce0b1afd7c535
boxofyellow <boxofyellow@github.com>
2020-04-16 16:36:28 -0400

    Get list of runs

--- index.js
+++ index.js
@@ -1,48 +1,107 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
+const { Octokit } = require('@octokit/action');
+const octokit = new Octokit()
 
-try {
-  const commandLine = core.getInput('commandLine');
-  const args = core.getInput('args');
-  const separator = core.getInput('argsSeparator');
+async function run() {
+    try {
+        const commandLine = core.getInput('commandLine');
+        const args = core.getInput('args');
+        const separator = core.getInput('argsSeparator');
+        const workflow = core.getInput('workflow');
 
-  const checkEveryRaw = core.getInput('checkEvery');
-  var checkEvery = Number(checkEveryRaw);
-  if (Number.isNaN(checkEvery) || !Number.isInteger(checkEvery) || checkEvery < 1)
-  {
-    core.setFailed(`checkEvery needs to be a integer, grater than 0, found ${checkEveryRaw}`);
-    return;
-  }
+        console.log(`Looking for workflow: ${workflow}`);
 
-  const detailLevelRaw = core.getInput('detailLevel');
-  var detailLevel = Number(detailLevelRaw);
-  if (Number.isNaN(detailLevel) || !Number.isInteger(detailLevel) || detailLevel < 0 || detailLevel > 2)
-  {
-    core.setFailed(`detailLevel needs to be a integer, grater than or equal to 0 and less then or equal to 2, found ${detailLevelRaw}`);
-    return;
-  }
+        const checkEveryRaw = core.getInput('checkEvery');
+        var checkEvery = Number(checkEveryRaw);
+        if (Number.isNaN(checkEvery) || !Number.isInteger(checkEvery) || checkEvery < 1)
+        {
+            core.setFailed(`checkEvery needs to be a integer, grater than 0, found ${checkEveryRaw}`);
+            return;
+        }
 
-  var argumentsToPass = null;
-  if (args)
-  {
-      argumentsToPass = args.split(separator);
-  }
+        const detailLevelRaw = core.getInput('detailLevel');
+        var detailLevel = Number(detailLevelRaw);
+        if (Number.isNaN(detailLevel) || !Number.isInteger(detailLevel) || detailLevel < 0 || detailLevel > 2)
+        {
+            core.setFailed(`detailLevel needs to be a integer, grater than or equal to 0 and less then or equal to 2, found ${detailLevelRaw}`);
+            return;
+        }
 
-  const options = {};
-  options.listeners = {
-    silent: detailLevel > 1
-  };
+        const maxRunsRaw = core.getInput('maxRuns');
+        var maxRuns = Number(maxRunsRaw);
+        if (Number.isNaN(maxRuns) || !Number.isInteger(maxRuns))
+        {
+            core.setFailed(`maxRuns needs to be a integer, found ${maxRunsRaw}`);
+            return;
+        }
 
-  var count = 0;
-  for (var i = 0; i < checkEvery; i++) {
-    count++;
-    if (detailLevel > 0)
-    {
-        console.log(`Starting ${count}`);
+        var argumentsToPass = null;
+        if (args)
+        {
+            argumentsToPass = args.split(separator);
+        }
+
+        const options = {};
+        options.listeners = {
+            silent: detailLevel > 1
+        };
+
+        const owner = github.context.payload.repository.owner.name;
+        const repo = github.context.payload.repository.name;
+
+        while (true)
+        {
+            var count = 0;
+            for (var i = 0; i < checkEvery; i++) {
+                count++;
+
+                if (maxRuns > 0 && count > maxRuns)
+                {
+                    if (detailLevel > 0)
+                    {
+                        console.log(`Reached ${count}`);
+                        return;
+                    }
+                    return;
+                }
+
+                if (detailLevel > 0)
+                {
+                    console.log(`Starting ${count}`);
+                }
+                await exec.exec(commandLine, argumentsToPass, options);
+            }
+
+            var result = await octokit.actions.listRepoWorkflowRuns({
+                owner: owner,
+                repo: repo,
+                workflow_file_name: workflow,
+                status: 'queued'
+            });
+
+            if (result.status !== 200)
+            {
+                const payload = JSON.stringify(result, undefined, 2);
+                core.setFailed(`Failed to read runs: ${payload}`);
+                return;
+            }
+
+            if (result.data.total_count > 0)
+            {
+                if (detailLevel > 1)
+                {
+                    const payload = JSON.stringify(result, undefined, 2);
+                    console.log(`Found at least queued item: ${payload}`);
+                }
+                return;
+            }
+        }
+
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    exec.exec(commandLine, argumentsToPass, options);
-  }
-
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
